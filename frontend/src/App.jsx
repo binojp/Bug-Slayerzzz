8f00877b4c852f1cb6b928462fc20cb3fbd88365
Savio <saviosaju737@gmail.com>
2025-08-08 19:36:09 +0530

    index,heatmap,app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Pages/Login";
 import Register from "./Pages/Register";
-
+import Heatmap from "./Pages/Heatmap"
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+                <Route path="/heatmap" element={<Heatmap />} />
+
+
       </Routes>
     </BrowserRouter>
   );
